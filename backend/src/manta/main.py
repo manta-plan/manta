9f0d00bbb6acb624c0fadd8d43fe8bd27d3f39f5
Kristijan Faust <kristijan.faust@openenergytransition.org>
2026-07-31 12:38:10 +0200

    feat: Setup FastAPI backend

--- backend/src/manta/main.py
+++ backend/src/manta/main.py
@@ -1,5 +1,20 @@
+import uvicorn
+from fastapi import FastAPI
+
+from manta.routes.health_route import router as health_router
+
+
+def create_app() -> FastAPI:
+    app = FastAPI(title="Manta")
+    app.include_router(health_router)
+    return app
+
+
+app = create_app()
+
+
 def main():
-    print("Hello from manta!")
+    uvicorn.run(app, host="127.0.0.1", port=8000)
 
 
 if __name__ == "__main__":
