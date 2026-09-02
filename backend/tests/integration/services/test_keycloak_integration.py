63d9a12a92f88fc235fcb70cf6ad0a784dc17b20
Shanti Chellaram <shanti.chellaram@openenergytransition.org>
2026-09-02 13:05:07 +0900

    fix tests

--- backend/tests/integration/services/test_keycloak_integration.py
+++ backend/tests/integration/services/test_keycloak_integration.py
@@ -1,16 +1,35 @@
-from keycloak import KeycloakAdmin, KeycloakOpenIDConnection
+import logging
+
+from keycloak import KeycloakAdmin, KeycloakOpenID, KeycloakOpenIDConnection
+
+logger = logging.getLogger(__name__)
 
 
 def test_connect(app_server: str) -> None:
     # Given
-    kc_connection = KeycloakOpenIDConnection(
+    kc_connection = KeycloakOpenID(
         server_url="http://localhost:8080",  # TODO Replace
         realm_name="manta",  # TODO replace these parameters with the realm-specific ones
         client_id="manta-client",
         client_secret_key="im-so-secret",  # noqa: S106
     )
 
-    kc_admin = KeycloakAdmin(connection=kc_connection)
+    # When
+    result = kc_connection.well_known()
+
+    # Then
+    assert result is not None
+
+
+def test_admin_connect(app_server: str) -> None:
+    # Given
+    kc_adm_connection = KeycloakOpenIDConnection(
+        server_url="http://localhost:8080",  # TODO Replace
+        realm_name="master",  # TODO replace these parameters with the realm-specific ones
+        username="kcadmin",
+        password="kcpassword",  # noqa: S106
+    )
+    kc_admin = KeycloakAdmin(connection=kc_adm_connection)
 
     # When
     result = kc_admin.get_admin_events()  # pyright: ignore[reportUnknownMemberType, reportUnknownVariableType]
